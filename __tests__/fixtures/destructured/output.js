e91dff2c9f5559a16446bdfe1761e56bba1ac2b9
Axel Hernández Ferrera <axelhzf@gmail.com>
2021-06-26 04:23:00 +0100

    fix: import lodash/last bug (#1)

--- __tests__/fixtures/destructured/output.js
+++ __tests__/fixtures/destructured/output.js
@@ -1,5 +1,6 @@
 import isEmpty from "lodash/isEmpty";
 import chunk from "lodash/chunk";
+import last from "lodash/last";
 const empty = {};
 const notEmpty = {
   a: 1
@@ -7,3 +8,4 @@ const notEmpty = {
 isEmpty(obj);
 isEmpty(notEmpty);
 chunk(["a", "b", "c", "d"], 2);
+last(["a", "b"]);
