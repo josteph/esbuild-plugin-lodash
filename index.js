345296a5e59082bda0f185960b0f3d1bdf1bc2bb
Ömer Faruk APLAK <omer@pankod.com>
2022-03-01 19:38:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function pluginLodashImport(options = {}) {
         const extension = path.extname(args.path).replace('.', '');
         const loader = JS_EXTENSIONS.has(extension) ? 'jsx' : extension;
 
-        const ramdaImportRegex =
-          /import\s+?(?:(?:(?:[\w*\s{},]*)\s+from\s+?)|)[\'\"](?:(?:lodash\/?.*?))[\'\"][\s]*?(?:;|$|)/g;
+        const ramdaImportRegex = /import\s+?(?:(?:(?:[\w*\s{},]*)\s+from\s+?)|)[\'\"](?:(?:lodash\/?.*?))[\'\"][\s]*?(?:;|$|)/g;
 
         const lodashImports = contents.match(lodashImportRegex);
 
